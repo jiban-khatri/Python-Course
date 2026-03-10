55c8f330898ca5b5d3aaf5bd2ad4fe4839271353
jiban-khatri <contact.jiban01@gmail.com>
2026-03-10 16:01:22 +0545

    string problems

--- Strings/main.py
+++ Strings/main.py
@@ -31,3 +31,44 @@ print(s[0:6:2]) #2 is step size i.e parts to skip
 
 print(s[-1:-5:-1])  
 
+print(s[::-1])
+
+
+
+
+# *******  Operations ********
+
+print('Jiban ' + 'Khatri')
+print("J.K " *50)
+
+
+print('delhi' == 'delhi')
+print('delhi' == 'Delhi')
+
+print("jiban" < "Jiban")
+
+
+'hello' or 'world'
+
+
+
+#  === String Methods ===
+
+print(len("jiban")) 
+print(max("jiban"))
+print(min('jiban'))
+
+print(sorted("jiban"))  # returns list
+
+
+s = "helLlo world"
+print(s.capitalize())   # capital first letter
+print(s.title())       # capitaize but all the first letter of words
+
+print(s.upper())    # converts to uppercase
+print(s.lower())    # converts to lowercase
+
+print(s.swapcase()) #converts uppercase to lowercase and lowercase to uppercase
+
+print(s.count('l'))
+print(s.find('w'))
